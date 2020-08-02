aecd4c39e10d73ce9d972dcda5cbdd882f649ea3
SidKolhapure <sid.kolhapure94@gmail.com>
2020-08-01 22:44:35 -0400

    added the demo code for redux thunk

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,17 +1,23 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 
 const initialState = {
-    articles: []
-  };
-  
-  function rootReducer(state = initialState, action) {
-    if (action.type === ADD_ARTICLE) {
+  articles: [],
+  remoteArticles: []
+};
 
-        return Object.assign({}, state, {
-            articles : state.articles.concat(action.payload)
-        })
-    }
-    return state;
-  };
-  
-  export default rootReducer;
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+
+  if (action.type === "DATA_LOADED") {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
+    });
+  }
+  return state;
+}
+
+export default rootReducer;
